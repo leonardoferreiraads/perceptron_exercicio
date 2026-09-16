a575bfec693d2ef713eafd915a675cbb16f1d817
Leonardo Ferreira <leonardodsf77@gmail.com>
2026-09-15 22:37:17 -0300

    Delete match directory

--- match/match4.py
+++
@@ -1,9 +0,0 @@
-idade = int(input("Digite a sua idade"))
-
-match idade:
-    case idade if idade <= 12:
-        print("Criança")
-    case idade if idade <= 17:
-        print("Adolescente")
-    case idade if idade >= 18:
-        print("Adulto")
